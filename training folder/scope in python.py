44a42703b2c7eff9de5be2216e0a33ac93bcffeb
amin soltani <aminsoltani.1221@gmail.com>
2023-07-08 16:19:39 +0330

    update scopes

--- training folder/scope in python.py
+++ training folder/scope in python.py
@@ -22,3 +22,19 @@ def func3():
 
 # func3()
 print(x)
+
+
+y= 7
+def test():
+    # print(y)
+    def test2():
+        x=3
+        print(x)
+        def test3():
+            nonlocal x
+            x = 6
+            print(x)
+        test3()
+        print(x)
+    test2()  
+test()
