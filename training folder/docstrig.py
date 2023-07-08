f25339adcd1b5376aea31eb80376174dec7817d3
amin soltani <aminsoltani.1221@gmail.com>
2023-07-08 10:59:29 +0330

    add docstring

---
+++ training folder/docstrig.py
@@ -0,0 +1,15 @@
+def max3(x:int, y:int, z:int):
+    """receives three number as input and return max of them
+    
+    Parameters:
+        x (int): An decimal integer
+        y (int): An decimal integer
+        z (int): An decimal integer
+    
+    Returns:
+        max_int (int): largest of them
+    """
+    return max(x, y, z)
+
+# print(max3.__doc__)
+print(help(max3))
