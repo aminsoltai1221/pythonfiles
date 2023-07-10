52b8a86cee255ecf83116f7acde688a825ce021d
amin soltani <aminsoltani.1221@gmail.com>
2023-07-10 11:12:17 +0330

    recursive func

---
+++ training folder/recursive func.py
@@ -0,0 +1,19 @@
+def fact(n):
+    if n == 1:
+        return 1
+    result = n * fact(n-1)
+    return result 
+
+print(fact(5))
+
+def fibonacci(n):
+    if n ==1:
+        return 1
+    elif n == 2:
+        return 1
+    result = fibonacci(n-1) + fibonacci(n-2)
+    return result
+
+for i in range(1,10):
+    print(fibonacci(i))
+    
