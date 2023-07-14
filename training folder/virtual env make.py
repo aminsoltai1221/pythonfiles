68ca29a7b948d17bfa163ce6e466857553a124a2
amin soltani <aminsoltani.1221@gmail.com>
2023-07-14 12:02:08 +0330

    pathes in python ,venv crete

---
+++ training folder/virtual env make.py
@@ -0,0 +1,17 @@
+"""create"""
+
+# python3 -m venv my_env  # linux
+# sudo python3 -m venv my_env  # linux
+
+
+
+"""activate"""
+# source my_env/bin/activate   # linux
+
+
+"""deactivate"""
+# deactivate
+
+"""test"""
+# which python   # linux  ====> .../my_env/bin/python   # linux
+
