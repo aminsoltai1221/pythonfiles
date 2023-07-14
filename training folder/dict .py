158e85096056ac53e7932de34ee7cc8a09491d9f
amin soltani <aminsoltani.1221@gmail.com>
2023-07-14 09:58:33 +0330

    changes in some files

--- training folder/dict .py
+++ training folder/dict .py
@@ -7,16 +7,28 @@
 #     print(item)
 
 my_dict={"name":"ali" ,"family":"alavi","age":"61"}
-print(my_dict)
-print(my_dict.items())
-print(my_dict.keys())
-print(my_dict.values())
-print(my_dict.popitem())
-print(my_dict)
-my_dict.clear()
-print(my_dict)
+my_dict2={"name2":"alireza" ,"family2":"alaa","age2":"56","c":6}
+
+# print(my_dict)
+# print(my_dict.items())
+# print(my_dict.keys())
+# print(my_dict.values())
+# print(my_dict.popitem())
+# print(my_dict)
 # del my_dict
 # print(my_dict)
 
 dict3=dict.fromkeys([i for i in range(4)],[5,6,7,8,3])
 print(dict3)
+print(my_dict|my_dict2)
+
+my_dict2["childrens"]=my_dict2.pop("c")
+print(my_dict2)
+my_dict.clear()
+
+
+l = ["ali", "hasan", "hossain"]
+
+dic4 = {name:[i for i in range(5)] for name in l}
+
+print(dic4)
--- training folder/warlus operator.py
+++ training folder/warlus operator.py
@@ -1,1 +1,15 @@
+from random import randrange   
+
+
+
 print(x:=54)  #  assign 54 to x and then print it
+
+l=[]
+while (s := input("pleae take your move!") != "exit"):
+    l.append(s)
+    
+    
+ 
+ 
+f = lambda :randrange(50,150)    
+print([l for i in range(10) if (l:=f())>100])
