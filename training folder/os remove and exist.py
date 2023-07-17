611c4a9139ffe3aac04515af8ccaa21877c38e42
amin soltani <aminsoltani.1221@gmail.com>
2023-07-17 11:15:36 +0330

    os remove and exist stdin and ...

---
+++ training folder/os remove and exist.py
@@ -0,0 +1,7 @@
+import os
+if os.path.exists("zfill.py"):
+    print("yes")
+
+
+if False:
+    os.remove("zfill.py")
---
+++ training folder/stdin stdout stderr.py
@@ -0,0 +1,9 @@
+import sys
+from sys import stdin, stderr, stdout
+
+f = stdin.readline()
+print(f)
+
+stdout.write("hello")
+
+stderr.write("Error")
