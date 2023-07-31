586ecc62426424330b64ae291557722f672efb5e
amin soltani <aminsoltani.1221@gmail.com>
2023-07-31 17:56:07 +0330

    exercise update

--- exercise/الگو  - سوال.py
+++ exercise/الگو  - سوال.py
@@ -1,5 +1,4 @@
-# number = "00??1100??1101?"
-cases = ["1??1"]
+cases = ["1?0??0?1"]
 num = cases[0]
 new_case = cases
 while True:
@@ -16,14 +15,8 @@ while True:
                 number1 = number[:number.index(i)] + "1" + number[number.index(i) + 1:]
                 number2 = number[:number.index(i)] + "0" + number[number.index(i) + 1:]
                 new_case.append(number1)
-                new_case.append(number2)
-                try:
-                    cases.remove(number)
-                except:
-                    pass
-
-
-# cases = [i for i in cases if "?" not in i]
-# cases.sort(key = lambda x: int(x))
+                new_case.append(number2)                
+                break
+            
 print(new_case)
-print(cases)
+print(len(set(new_case)))
