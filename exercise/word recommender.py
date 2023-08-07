d096a0631f68c59aff6c4f03c9fe970edaa59887
amin soltani <aminsoltani.1221@gmail.com>
2023-08-07 21:56:39 +0330

    edit

--- exercise/word recommender.py
+++ exercise/word recommender.py
@@ -1,33 +1,23 @@
-words_list = ["assert", "can", "candy"]
 
 
-with open("exercise/listofwords.txt") as wf:
-    result = wf.readlines()
-    for i in result:
-        words_list.append(i[:-1])
-    
-with open("listedwords", "w") as lw:
-    lw.write(str(words_list))
-    
-print(words_list) 
 
 
-# def recommender(word):
-#     l = [ ]
-#     l2 = [ ]
-#     for words in words_list:
-#         if words == word:
-#             l.append(words)
-#         if words.startswith(word):
-#             l.append(words)
-#         if abs(len(word) - len(words)) < 3:
-#             l2.append(words)
-#     for words in l2:
-#         word1 = set([i for i in word])
+def recommender(word):
+    l = [ ]
+    l2 = [ ]
+    for words in words_list:
+        if words == word:
+            l.append(words)
+        if words.startswith(word):
+            l.append(words)
+        if abs(len(word) - len(words)) < 3:
+            l2.append(words)
+    for words in l2:
+        word1 = set([i for i in word])
             
         
-#     return l
-# print(recommender("can"))
+    return l
+print(recommender("can"))
             
             
          
