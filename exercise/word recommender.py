55e3e1d6261bce09f21ea83fa738049e89624900
amin soltani <aminsoltani.1221@gmail.com>
2023-08-08 05:57:07 +0330

    finall in recommender

--- exercise/word recommender.py
+++ exercise/word recommender.py
@@ -15,19 +15,27 @@ def recommender(word):
         word1 = set([i for i in word])
         word2 = set([i for i in words])
         if len(word1 - word2) < 2 and len(word2 - word1) <2:
-            max_min = [words, word]
-            max_min.sort(key=lambda x:len(x))
-            min,max = max_min[0],max_min[1]
+            if len(word) != len(words):
+                max_min = [words, word]
+                max_min.sort(key=lambda x:len(x))
+                min,max = max_min[0],max_min[1]
+            else:
+                min, max = word,words
+                
             m = 0
             for i in range(len(min)):
                 if min[i] != max[i]:
                     m += 1
-                if m > 2:
+                if len(max) <5 and m > 1:
+                    break
+                if len(words) >4 and m > 1:
+                    break
+                if words == min and m == 1:
                     break
             else:    
                 l.append(words)
-    return l
-print(recommender("can"))
+    return list(set(l))
+print(recommender("word"))
             
             
          
