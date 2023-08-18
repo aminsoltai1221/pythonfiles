a5bd54c2219ff6aa0e9a669234c8086341dea64f
amin soltani <aminsoltani.1221@gmail.com>
2023-08-18 16:37:49 +0330

    add khayyam

---
+++ training folder/khayyam lib .py
@@ -0,0 +1,2 @@
+from khayyam import *
+print(JalaliDatetime.now().strftime("%y-%B-%d  %A  %H:%M"))
--- training folder/requirements create auto.py
+++ training folder/requirements create auto.py
@@ -1,1 +1,2 @@
-# pip freeze > requirements.txt   terminal
+# pip freeze > requirements.txt   in terminal   ---> to save libraries in a file
+# pip install -r requirements.txt   ////////    ---> to install all libraries that used
