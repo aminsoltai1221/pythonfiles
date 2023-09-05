a8c67ebce965e3b8691523ffc0915c0dedf7a0e1
aminsoltai1221 <aminsoltani.1221@gmail.com>
2023-09-05 16:00:09 +0330

    dict by zip and ...

--- exercise/qouera/michel jordan.py
+++ exercise/qouera/michel jordan.py
@@ -1,11 +1,7 @@
 text = input("please inpur your string : ")
-text_reverse = text[::-1]
-print(text_reverse)
-new_text = ""
-for i in range(len(text)):
-    if text_reverse[i] == "x":
-        new_text = text_reverse[i+1:][::-1] + text_reverse[0:i+1]
-        
+new_text = text 
+for i in range(1,len(text)+1):
+    i = -1 * i
+    if new_text[i] == "x":
+        new_text = new_text[:i][::-1] + new_text[i:]     
 print(new_text)
-
-    
