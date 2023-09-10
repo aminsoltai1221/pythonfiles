2c97d193183a4d32a19c6fc7700e1fb37a1bd355
aminsoltai1221 <aminsoltani.1221@gmail.com>
2023-09-10 20:26:07 +0330

    update

--- training folder/dict comprehension example.py
+++ training folder/dict comprehension example.py
@@ -1,3 +1,11 @@
 my_dict={"name":"ali" ,"family":"alavi","age":61}
 d2 = {k:v for k,v in my_dict.items() if type(v) == int}
 print(d2)
+
+
+
+
+l1 = [1, 2, 3, 4]
+l2 = ["ali", "hasan", "hossain"]
+d2 = {k:v for k,v in zip(l1, l2) if not type(v) == int}
+print(d2)
